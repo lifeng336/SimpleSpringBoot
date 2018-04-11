eadac54e387555652b6246bc5ca61972f907c928
lifeng336 <ns-ltq@hotmail.com>
2018-04-11 16:52:36 +0800

    修改swagger

--- SimpleSpringBoot/src/main/java/hello/controller/HelloController.java
+++ SimpleSpringBoot/src/main/java/hello/controller/HelloController.java
@@ -1,22 +1,25 @@
 package hello.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-
+@Api(value="第一个实例")
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-	@ApiOperation(value="hello",notes="hello")
+	@ApiOperation(value="登陆",notes="登陆")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name="name",value="userName",required=true,dataType="String",paramType="UserInfo"),
-		@ApiImplicitParam(name="pass",value="passWord",required=true,dataType="String")
+		@ApiImplicitParam(name="name",value="用户名",required=true,dataType="String",paramType="query"),
+		@ApiImplicitParam(name="pass",value="密码",required=true,dataType="String",paramType="query")
 	})
-    @RequestMapping("/")
-    public String hello(){
-        return "Greetings from Spring Boot!";
+    @RequestMapping(value="/login",method= {RequestMethod.POST})
+    public String login(String name,String pass){
+        return "name="+name+",password="+pass;
     }
 }
