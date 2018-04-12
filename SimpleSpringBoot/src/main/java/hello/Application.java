24d4edf4b987b092cc31d8ee943a3792f80db52c
lifeng336 <ns-ltq@hotmail.com>
2018-04-12 09:42:05 +0800

    local

--- SimpleSpringBoot/src/main/java/hello/Application.java
+++ SimpleSpringBoot/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Application {
 
     public static void main(String[] args) {
-        // http://localhost:8080/
+        // http://localhost:8080/rrr
         SpringApplication.run(Application.class, args);
     }
 }
