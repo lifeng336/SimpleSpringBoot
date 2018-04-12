10eeb14d86410d7857b31b68b17dbe45766e5858
lifeng336 <ns-ltq@hotmail.com>
2018-04-12 10:31:09 +0800

    branch Test

---
+++ SimpleSpringBoot/src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class Application {
+
+    public static void main(String[] args) {
+        // http://localhost:8080/ttt
+        SpringApplication.run(Application.class, args);
+    }
+}
